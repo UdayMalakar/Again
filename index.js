140252b3fb57d8da7b40051da5c22afaefc3b597
UdayMalakar <147069723+UdayMalakar@users.noreply.github.com>
2024-03-10 19:06:45 +0530

    third

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express=require("express");
 const app=express();
 require("dotenv").config();
 const dbconnect=require("./config/database");
+const cors =require("cors");
 dbconnect();
 const router=require("./routes/router");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use("/api/v1",router);
